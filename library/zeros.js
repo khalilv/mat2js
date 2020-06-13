c9708a5d08fb9af415c67d63511c7b2df9422ba0
khalilv <khalil_virji@hotmail.com>
2020-06-13 17:15:11 -0400

    modifications

--- library/zeros.js
+++ library/zeros.js
@@ -7,29 +7,21 @@ module.exports = {
 };
 
 function get_type(params_str) {
-    let type = 0;
-    let params;
+    let type = -1;
     if (params_str[0] == "[" && params_str[params_str.length - 1] == "]") {
-        params_str = params_str.slice(params_str.indexOf("[") + 1, params_str.lastIndexOf("]"));
-        params = params_str.trim().split(' ').map(x => x.trim());
-        if (params.length > 2) {
+        if (params_str.slice(params_str.indexOf("[") + 1, params_str.lastIndexOf("]")).trim().split(' ').map(x => x.trim()).length > 2) {
             type = 2;
-        } else if (params.length > 1) {
+        } else {
             type = 1;
         }
     } else if (params_str.includes(",")) {
-        params = params_str.trim().split(',').map(x => x.trim());
-        if (params.length > 2) {
+        if (params_str.trim().split(',').map(x => x.trim()).length > 2) {
             type = 2;
         } else {
             type = 1;
         }
     } else if (/^\d+$/.test(params_str)) {
         type = 0;
-        params = [parseInt(params_str)];
-    } else {
-        type = -1;
-        params = null;
     }
-    return [type, params];
+    return type;
 }
