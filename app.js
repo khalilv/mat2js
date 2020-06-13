e13fbf44a74c885f6fe7ad100fbe85581564cd66
khalilv <khalil_virji@hotmail.com>
2020-06-13 12:26:50 -0400

    added first page

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+/* GET home page. */
+app.get('/', function (req, res, next) {
+    res.render('index.ejs', { title: 'Express' });
+});
+
+/* GET users listing. */
+app.get('/users', function (req, res, next) {
+    res.send('Reached new page');
+});
+
+/* sample HTTP call */
+app.get('/sample', function (req, res, next) {
+    res.json({success : true, body : "HTTP request successful"}); 
+});
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
